3eaf872a47d7c2625c09315e244f12eefe3e6919
Sen ZmaKi <phidelalvin@gmail.com>
2025-06-09 07:34:11 +0300

    move_zeroes: Add question number

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,3 +1,4 @@
+# 283. Move Zeroes
 class Solution:
     def moveZeroes(self, nums: list[int]) -> None:
         """
@@ -8,28 +9,24 @@ class Solution:
             if num == 0:
                 zeroes_count += 1
         no_zeroes = [num for num in nums if num != 0]
-        no_zeroes.extend([0]*zeroes_count)
+        no_zeroes.extend([0] * zeroes_count)
         nums.clear()
         nums.extend(no_zeroes)
 
     """
     Two pointers problem
     """
+
     def moveZeroesCorrect(self, nums: list[int]) -> None:
         left = 0
         for right, num in enumerate(nums):
-            if num != 0 :
+            if num != 0:
                 if nums[left] == 0:
                     nums[left], nums[right] = nums[right], nums[right]
                 left += 1
 
 
-
-
-
-
 sol = Solution()
-nums = [1,3,12]
+nums = [1, 3, 12]
 sol.moveZeroesCorrect(nums)
 print(nums)
-        
