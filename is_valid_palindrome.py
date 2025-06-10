aa5e1fa1e71960417762e4b37590b6484d470aca
Sen ZmaKi <phidelalvin@gmail.com>
2025-06-10 11:05:25 +0300

    Add is_valid_palindrome

---
+++ is_valid_palindrome.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        s = s.lower()
+        while left < len(s) and right >= 0:
+            left_char = s[left]
+            right_char = s[right]
+            if not left_char.isalnum():
+                left += 1
+                continue
+            if not right_char.isalnum():
+                right -= 1
+                continue
+            if left_char != right_char:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+    # Two pointers problem
+    def isPalindromeCorrect(self, s: str) -> bool:
+        clean_s = "".join(char.lower() for char in s if char.isalnum())
+        left = 0
+        right = len(clean_s) - 1
+        while left < right:
+            if clean_s[left] != clean_s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+
+sol = Solution()
+s = "raceca*()r"
+result = sol.isPalindromeCorrect(s)
+print(result)
